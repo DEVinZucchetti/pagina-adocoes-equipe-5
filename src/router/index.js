5434280d83b1897d2a5f8b3ea18a4864d5427ddd
ArthurPerozin <135287664+ArthurPerozin@users.noreply.github.com>
2024-02-10 22:43:21 -0300

    Feat: configura rotas

--- src/router/index.js
+++ src/router/index.js
@@ -1,40 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import Main from '../views/Main.vue'
-import Home1 from '../views/pessoa1/Home.vue'
-import Home2 from '../views/pessoa2/Home.vue'
-import Home3 from '../views/pessoa3/Home.vue'
-import Home4 from '../views/pessoa4/Home.vue'
+import Home from '../views/pessoa1/HomeCTA.vue'
+import Pets from '../views/pessoa1/PetsView.vue'
+import PetsProfile from '../views/pessoa1/PetsProfile.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'main',
-      component: Main
+      name: 'Home',
+      component: Home
     },
     {
-      path: '/home1',
-      name: 'home1',
-      component: Home1
+      path: '/pets-adocao',
+      name: 'pets',
+      component: Pets
     },
     {
-      path: '/home2',
-      name: 'home2',
-      component: Home2
+      path: '/pets-adocao/:id/perfil',
+      name: 'PetsProfile',
+      component: PetsProfile
     },
-    {
-      path: '/home3',
-      name: 'home3',
-      component: Home3
-    },
-    {
-      path: '/home4',
-      name: 'home4',
-      component: Home4
-    },
-   
   ]
 })
 
