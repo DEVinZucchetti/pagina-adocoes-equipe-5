c7aa654c7ca8ab047b0f15fb43a7f8ae1b98000a
joseCdia <josecdia@hotmail.com>
2024-02-13 15:53:19 -0300

    Criando pagina home e pagina de listagem de pets cadastrados

--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,10 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import Main from '../views/Main.vue'
 import Home1 from '../views/pessoa1/Home.vue'
-import Home2 from '../views/pessoa2/Home.vue'
+import Home2 from '../views/pessoa2/HomeCTA.vue'
 import Home3 from '../views/pessoa3/Home.vue'
 import Home4 from '../views/pessoa4/Home.vue'
+import PetsList from '../views/pessoa2/PetsList.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -33,6 +34,11 @@ const router = createRouter({
       path: '/home4',
       name: 'home4',
       component: Home4
+    },
+    {
+      path: '/pets-adocao',
+      name: 'PetsList',
+      component: PetsList
     },
    
   ]
